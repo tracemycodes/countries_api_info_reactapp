101680e390472afce48fbd685c3b66495d98fa3a
Agba Emmanuel <agbaayoh@gmail.com>
2022-04-25 10:51:39 -0700

    form field for search and country region

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 const NavBar = styled.header`
   display: flex;
@@ -11,18 +11,35 @@ const NavBar = styled.header`
   box-shadow: 0px 10px 15px 3px rgba(0, 0, 0, 0.1);
   color: #fff;
   z-index: 3;
-`
+
+  button {
+    border: none;
+    display: flex;
+    font-size: 0.7rem;
+    background-color: hsl(209, 23%, 22%);
+    color: #fff;
+    cursor: pointer;
+  }
+
+  .theme-icon {
+    margin-right: 0.5rem;
+  }
+
+  .div p {
+    margin-left: 0.4rem;
+  }
+`;
 
 const Header = () => {
   return (
     <NavBar>
       <h1>Where in the world?</h1>
-        <button class="theme-btn">
-          <i class="fa-regular fa-sun theme-icon"></i>
-          <p>Dark Mode</p>
-        </button>
+      <button class='theme-btn'>
+        <i class='fa-regular fa-sun theme-icon'></i>
+        <p>Dark Mode</p>
+      </button>
     </NavBar>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
