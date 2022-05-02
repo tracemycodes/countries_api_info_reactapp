7cd3e6dc72a5c2d937568a5214e434956ba164ee
Agba Emmanuel <agbaayoh@gmail.com>
2022-05-02 09:16:36 -0700

    router insatalled, switch replaced with routes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import CountrySection from "./components/countries_section/CountrySection";
-import Header from "./components/Header";
-
+import { useState, Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CountrySection from './components/countries_section/CountrySection';
+import Header from './components/Header';
 
 function App() {
   const [countriesArr, setCountriesArr] = useState([]);
 
-   const getCountryInfo = async () => {
+  const getCountryInfo = async () => {
     const countryResponse = await fetch('https://restcountries.com/v2/all'),
+      countryData = await countryResponse.json();
 
-    countryData = await countryResponse.json();
-
-    setCountriesArr(countryData)
-   }
+    setCountriesArr(countryData);
+  };
 
   return (
     <Router>
-    <div className="App">
-      <Header/>
-      <Switch>
-        <Route exact path='/' render={props => (
-          <CountrySection getCountryInfo={getCountryInfo} countriesArr={countriesArr} />
-
-        )} />
-      
-      </Switch>
-    </div>
+      <div className='App'>
+        <Header />
+        <Routes>
+          <Route
+            exact
+            path='/'
+            render={(props) => (
+              <>
+                <CountrySection
+                  getCountryInfo={getCountryInfo}
+                  countriesArr={countriesArr}
+                />
+              </>
+            )}
+          />
+        </Routes>
+      </div>
     </Router>
   );
 }
