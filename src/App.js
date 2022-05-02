d41f73f774057159c7c54e51c39577ced4481d50
Agba Emmanuel <agbaayoh@gmail.com>
2022-05-02 10:15:54 -0700

    single component route initialed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, Fragment } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useParams } from 'react-router-dom';
 import CountrySection from './components/countries_section/CountrySection';
+import SingleCountry from './components/countries_section/SingleCountry';
 import Header from './components/Header';
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
                 countriesArr={countriesArr}
               />}
           />
+          <Route exact path='/country/:id' element={ <SingleCountry/> } />
         </Routes>
       </div>
     </Router>
