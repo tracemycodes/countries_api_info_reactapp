dc2ec6280ce653a86fda5a3e349e99850426d920
Agba Emmanuel <agbaayoh@gmail.com>
2022-05-03 21:09:18 -0700

    single country styling

--- src/components/countries_section/SingleCountry.js
+++ src/components/countries_section/SingleCountry.js
@@ -10,12 +10,15 @@ const CountryDetails = styled.div`
   width: calc(100% - 6rem);
   height: calc(100vh - 4rem);
   background-color: hsl(207, 26%, 17%);
-  }
+}
  
   button {
   padding: .5rem 2rem;
   margin: 1rem 0 2rem;
   box-shadow: 0px 5px 5px 3px rgba(0, 0, 0, 0.1);
+  background-color: hsl(209, 23%, 22%);
+  border: none;
+  color: #fff;
   cursor: pointer;
 
     .fa-solid {
@@ -35,41 +38,104 @@ const CountryDetails = styled.div`
       box-shadow: 0px 5px 5px 3px rgba(0, 0, 0, 0.1);
     }
   }
+
+  > div {
+  width: 100%;
+  height: calc(100% - 5rem);
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(18rem, 1fr));
+  gap: 2rem;
+}
+
+ article {
+  box-shadow: unset;
+
+  h2 {
+  font-size: 0.85rem;
+  margin: 0.8rem;
+  }
+
+  p {
+  font-size: 0.7rem;
+  margin: 0.3rem 0.8rem 0.3rem 0.8rem;
+  margin-bottom: 0.3rem;
+  }
+}
+ 
+
 .single-country-details .fl-left *+*,
 .single-country-details .fl-right *+* {
   margin-top: .6rem;
 }
-.single-country-details p, .single-country-details h2 {
-  margin: unset;
-}
-.single-country-details p {
-  font-size: .8rem;
-}
-.single-country-details h2 {
-  font-size: 1rem;
-  font-weight: bold;
-  margin-top: 2rem;
+.single-country-details{
+
+  color: #fff;
+
+  p{ 
+    margin: unset;
+    font-size: .8rem;
+  }
+
+  h2 {
+    margin: unset;
+    font-size: 1rem;
+    font-weight: bold;
+    margin-top: 2rem;
+  }
+
+  .fl-left, .fl-right {
+    margin-top: 1.4rem;
+  }
+  .fl-down p {
+    font-size: 1rem;
+    font-weight: bold;
+    padding-bottom: 1rem;
+  }
+  .fl-down {
+    padding: unset;
+    margin: 1.4rem 0;
+  }
+  .fl-down span {
+    margin-right: .3rem;
+    font-size: .8rem;
+    padding: .25rem .4rem;
+    border: 1px solid hsl(206, 12%, 78%);
+    border-radius: 3px;
+  }
+  @media (min-width: 966px) {
+    .single-country-display>div {
+    gap: 7rem;
+    }
+    .fl-left {
+      float: left;
+    }
+
+    .fl-right {
+      float: right;
+    }
+
+    .fl-down {
+      clear: both;
+      padding-top: 2rem;
+    }
+
+    .fl-down p {
+      display: inline;
+    }
+
+    .fl-down div {
+      display: inline;
+    }
+  }
 }
 
-.fl-left, .fl-right {
-  margin-top: 1.4rem;
+
+@media (min-width: 966px) {
+  >div {
+    gap: 7rem;
+  }
 }
-.fl-down p {
-  font-size: 1rem;
-  font-weight: bold;
-  padding-bottom: 1rem;
-}
-.fl-down {
-  padding: unset;
-  margin: 1.4rem 0;
-}
-.fl-down span {
-  margin-right: .3rem;
-  font-size: .8rem;
-  padding: .25rem .4rem;
-  border: 1px solid hsl(206, 12%, 78%);
-  border-radius: 3px;
-}
+
 
 `;
 
@@ -95,7 +161,7 @@ const SingleCountry = ({ presentCountry }) => {
               <i className='fa-solid fa-left-long' /> Back
           </button>
         </Link>
-      <div gd-2=''>
+      <div>
         <article className='single-country-flag'>
           <div className='single-flag'>
             <img src={flag} alt='' />
